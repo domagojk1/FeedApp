e854086cb9e557f87b31d8320e389216dc4bf78e
domagojkopic <kopicdomagoj@gmail.com>
2021-09-15 19:01:47 +0200

    Project setup

---
+++ FeedApp/FeedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FeedApp
+//
+//  Created by Domagoj on 15.09.2021..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
